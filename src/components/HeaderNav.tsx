02db70d4653718452a8e46e852d44fc610bc6b6b
Ajay Rawat <coderajay86091@gmail.com>
2026-09-13 08:58:18 +0530

    Revert "feat: add store financial graph in header (expenses, loss, profit, sales across daily/weekly/monthly/yearly)"
    This reverts commit 095caba690da048afeb8164b06dd54fc8f71aa8e.

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Link, useRouterState } from "@tanstack/react-router";
 import { BusinessMemoryDialog } from "@/components/BusinessMemoryDialog";
 import { EveningReconciliationModal } from "@/components/EveningReconciliationModal";
-import { HeaderFinancialGraph } from "@/components/HeaderFinancialGraph";
 import { LanguageToggle } from "@/components/LanguageToggle";
 import { PaytmLoyaltyModal } from "@/components/PaytmLoyaltyModal";
 import { VoiceToLedgerModal } from "@/components/VoiceToLedgerModal";
@@ -163,9 +162,6 @@ export function HeaderNav() {
             </span>
           </div>
         </div>
-
-        {/* Global Store Financial Pulse & Graph Bar */}
-        <HeaderFinancialGraph />
       </header>
 
       {/* Mobile Bottom Navigation Bar */}
